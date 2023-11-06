bb04b13a6d168ba75ed0906f81b0734c7d3cfae7
kimjaewoo0513 <howchamsin@gmail.com>
2023-11-06 20:55:06 +0900

    두번째 커밋

---
+++ auth/src/main/java/com/demo/auth/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.demo.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+        .csrf().disable()
+        .cors()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/user/**").permitAll();
+		return http.build();
+		
+	}
+
+}
